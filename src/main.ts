55ed7f317e2212241a7bc18fdda771a70d0dd206
Manjot <noctis.hive@gmail.com>
2025-06-30 03:51:05 +0530

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://nitappbackend.onrender.com', 
+    origin: 'https://nitapp-ysdi.vercel.app', 
     credentials: true,
   });
   app.use(cookieParser());
